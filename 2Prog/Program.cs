400be3cc474bea4e7738136af95fd1c12b50be42
Anatoly <up.many.money@gmail.com>
2022-09-02 01:16:54 +0300

    Create two function

---
+++ 2Prog/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число
+// и выдаёт количество цифр в числе.
+
+Console.WriteLine("Enter number:");
+int num = int.Parse(Console.ReadLine());
+
+int QuantityNumbers(int num1)
+{
+int fig = 0;
+
+while (num1 > 0)
+{
+    fig++;
+    num1 = num1 / 10;
+}
+
+
+return (fig);
+}
+
+Console.WriteLine(QuantityNumbers(num));
