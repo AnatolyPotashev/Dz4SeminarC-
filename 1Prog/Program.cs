81f53400ae7cd3ae226680480c7d50af1dd04fe7
Anatoly <up.many.money@gmail.com>
2022-09-02 00:30:47 +0300

    Copy one programm

---
+++ 1Prog/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А
+
+Console.WriteLine("Введите число:");
+int num = int.Parse(Console.ReadLine());
+
+int GetSum(int numA)
+{
+int sum = 0;
+
+while (numA > 0)
+{
+    sum = sum + numA;
+    numA--;
+}
+
+    return (sum);
+}
+
+Console.WriteLine(GetSum(num));
