23cd84f84a0dba9972481dca84cd1bfaff499331
Anatoly <up.many.money@gmail.com>
2022-09-02 01:52:40 +0300

    Create ThreeProgramm

---
+++ 3Prog/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число N
+// и выдаёт произведение чисел от 1 до N.
+
+Console.WriteLine("Enter number:");
+int num = int.Parse(Console.ReadLine());
+
+int MultNumber(int num1)
+{
+
+int mult = 1;
+while (num1 > 0)
+{
+    mult = num1 * mult;
+    num1--;
+}
+
+
+return mult;
+}
+Console.WriteLine(MultNumber(num));
