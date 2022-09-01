5ccea51904cb5ad7901f0dd576b9606cacca9a0c
Anatoly <up.many.money@gmail.com>
2022-09-02 02:49:42 +0300

    Create four programm

---
+++ 4Prog/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов, 
+// заполненный нулями и единицами 
+// в случайном порядке.
+
+int[] array = new int[8];
+foreach (int num in array)
+{
+    Random rnd = new Random();
+    int rnd2 = rnd.Next(0, 2);
+    Console.Write(rnd2 + ", ");
+}
