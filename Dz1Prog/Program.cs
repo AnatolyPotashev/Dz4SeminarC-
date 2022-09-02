485902d079fce6f24750f0121ec6a14877d767e8
Anatoly <up.many.money@gmail.com>
2022-09-02 17:19:41 +0300

    Work dz 1 prog

---
+++ Dz1Prog/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите цикл, 
+// который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+
+Console.WriteLine("Enter number A:");
+double numA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter number B:");
+double numB = Convert.ToInt32(Console.ReadLine());
+
+double result1 = 1;
+int levelNumbers(double result1)
+{
+// int i = 1;
+double result = 1;
+for (int i = 1; i <= numB; i++)
+{
+    result = result*numA;
+
+    return result;
+}
+
+return result;
+}
+Console.WriteLine(levelNumbers(result1));
